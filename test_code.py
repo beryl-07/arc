a8960e7a0303c2b14762f3b44a4d8d767cac5f68
Béryl HOUESSOU <22227081-beryl.hoe@users.noreply.gitlab.com>
2026-09-09 17:41:39 +0100

    Update SFT training script

--- test_code.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-url = "https://raw.githubusercontent.com/huggingface/trl/main/trl/trainer/sft_config.py"
-req = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(req) as response:
-        content = response.read().decode('utf-8')
-        print("chunked_lm_head" in content)
-except Exception as e:
-    print(e)
--- test_trl2.py
+++
@@ -1,6 +0,0 @@
-import torch
-from transformers import AutoModelForCausalLM, TrainingArguments
-from peft import LoraConfig, get_peft_model
-from trl import SFTTrainer, SFTConfig
-
-print(hasattr(SFTConfig, "chunked_lm_head"))
